83e9d96dffabd6f5fe04843b4e2490fafeb85b66
Ybalrid <ybalrid@cecmu.org>
2015-04-04 20:10:58 +0200

    application minimale avec Ogre

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
-main(){}
+#include <Ogre.h>
+
+int main()
+{
+    Ogre::Root* root = new Ogre::Root("plugins.cfg", "ogre.cfg", "ogre.log");
+    
+    //va chercher la config de ogre dans ogre.cfg
+    if(!root->restoreConfig())
+        //affiche la fenetre de config de ogre
+        if(!root->showConfigDialog()) 
+            //explosion nucléaire
+            return -1;
+
+    
+    Ogre::RenderWindow* window = root->initialise(true, "The most epic window of the universe");
+    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+    Ogre::SceneManager* smgr = root->createSceneManager("DefaultSceneManager", "Le Scene Manager");
+    Ogre::Camera* cam = smgr->createCamera("camera");
+    Ogre::Viewport* vp = window->addViewport(cam);
+    //Cette couleur n'est pas Gay du tout : 
+    vp->setBackgroundColour(Ogre::ColourValue(1,0,1));
+
+    while(true)
+        root->renderOneFrame();
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
+IFLAGS= -I/usr/include/OGRE -I/usr/include/boost/
+LDFLAGS=  -L/usr/lib/x86_64-linux-gnu -lboost_system -lOgreMain -lOIS -lpthread
+
 all:
-	g++ main.cpp -o test
+	g++  main.cpp -o test $(IFLAGS) $(LDFLAGS)
